0ad97d7c59ee0951cc2302abc56ad8c92841cb02
zhangyu <zy2071@126.com>
2020-10-06 22:01:24 +0800

    feat：集成 dva, 添加路由以及 redux 的基础用例

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import dva from "dva";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// 1. Initialize
+const app = dva();
+
+// 2. Plugins
+// app.use({});
+
+// 3. Model
+app.model(require("./example").default);
+
+// 4. Router
+app.router(require("./router").default);
+
+// 5. Start
+app.start("#root");
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
